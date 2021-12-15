bfc142adb99b63dc2fbe66d84ed94dab341bf318
Roman Kotelnikov <46915986+KotelnikovRoman@users.noreply.github.com>
2021-12-15 10:29:11 +0800

    нарисовал человечка в convas

--- js_for_children/js/13_canvas.js
+++ js_for_children/js/13_canvas.js
@@ -9,11 +9,14 @@ for(var i = 0; i < 8; i++) {
 */
 
 let Robot = function(arr_head, arr_neck, arr_torso, arr_ra, arr_la, arr_rl, arr_ll) {
+    //цвет рисования
+    ctx.fillStyle = "orange";
     this.head(arr_head);
     this.neck(arr_neck);
     this.torso(arr_torso);
     this.right_arm(arr_ra);
     this.left_arm(arr_la);
+    ctx.fillStyle = "#DEB887";
     this.right_leg(arr_rl);
     this.left_leg(arr_ll);
 };
@@ -62,6 +65,7 @@ left_arm();
 right_leg();
 left_leg();
 */
+//coordinats for robot
 let arr_head = [75, 10, 50, 50];
 let arr_neck = [95, 60, 10, 30];
 let arr_torso = [65, 90, 70, 70];
@@ -69,4 +73,74 @@ let arr_right_arm = [35, 90, 30, 10];
 let arr_left_arm = [135, 90, 30, 10];
 let arr_right_leg = [65, 160, 10, 30];
 let arr_left_leg = [125, 160, 10, 30];
-let Valli = new Robot(arr_head, arr_neck, arr_torso, arr_right_arm, arr_left_arm, arr_right_leg, arr_left_leg);
+/**
+ * создаёт и рисует объект Валли
+ */
+//let Valli = new Robot(arr_head, arr_neck, arr_torso, arr_right_arm, arr_left_arm, arr_right_leg, arr_left_leg);
+
+/*
+ctx.fillStyle = "#0000FF";
+ctx.fillRect(0, 0, 50, 200);
+ctx.fillStyle = "#7FFF00";
+ctx.fillRect(50, 0, 50, 200);
+ctx.fillStyle = "#DC143C";
+ctx.fillRect(100, 0, 50, 200);
+ctx.fillStyle = "#BDB76B";
+ctx.fillRect(150, 0, 50, 200);
+*/
+let my_color = () => {
+    ctx.fillStyle = "#F0F8FF";
+    ctx.fillRect(0, 0, 200, 65);
+    ctx.fillStyle = "blue";
+    ctx.fillRect(0, 65, 200, 65);
+    ctx.fillStyle = "red";
+    ctx.fillRect(0, 130, 200, 65);
+}
+
+let rectangle = () => {
+    //color
+    ctx.strokeStyle = "DeepPink";
+    //size line
+    ctx.lineWidth = 4;
+    //rectangle
+    ctx.strokeRect(10, 10, 100, 20);
+}
+
+let cross = () => {
+    ctx.strokeStyle = "Turquoise";
+    ctx.lineWidth = 4;
+    ctx.beginPath();
+    ctx.moveTo(10, 10);
+    ctx.lineTo(60, 60);
+    ctx.moveTo(10, 60);
+    ctx.lineTo(60, 10);
+    ctx.stroke();
+};
+
+let man = (width, height) => {
+    let center_x = width / 2;
+    let center_y = height / 2;
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.moveTo(center_x - 15, center_y - 25);
+    ctx.lineTo(center_x + 15, center_y - 25);
+    ctx.moveTo(center_x - 15, center_y - 25);
+    ctx.lineTo(center_x - 15, center_y + 5);
+    ctx.moveTo(center_x - 15, center_y + 5);
+    ctx.lineTo(center_x + 15, center_y + 5);
+    ctx.moveTo(center_x + 15, center_y + 5);
+    ctx.lineTo(center_x + 15, center_y - 25);
+    ctx.moveTo(center_x, center_y + 5);
+    ctx.lineTo(center_x, center_y + 55);
+    ctx.moveTo(center_x, center_y + 15);
+    ctx.lineTo(center_x - 30, center_y + 5);
+    ctx.moveTo(center_x, center_y + 15);
+    ctx.lineTo(center_x + 30, center_y + 5);
+    ctx.moveTo(center_x, center_y + 55);
+    ctx.lineTo(center_x - 10, center_y + 90);
+    ctx.moveTo(center_x, center_y + 55);
+    ctx.lineTo(center_x + 10, center_y + 90);
+    ctx.stroke();
+};
+
+man(200, 200);
