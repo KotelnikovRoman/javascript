57030be25d193f355c9041d47d46e4bcae2808d0
Roman Kotelnikov <46915986+KotelnikovRoman@users.noreply.github.com>
2022-01-09 17:49:02 +0800

    upgrade 13_canvas.js

--- js_for_children/js/13_canvas.js
+++ js_for_children/js/13_canvas.js
@@ -330,3 +330,18 @@ for (let item of points) {
     console.log(item[0]+ " " +item[1]);
 }
 */
+
+pointing = (obj) => {
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = 2;
+    $("html").mousemove(function(event) {
+        //$(obj).offset({});
+        x = event.clientX;
+        y = event.clientY;
+        ctx.beginPath();
+        ctx.moveTo(x, y)
+        ctx.lineTo(x + 1, y + 1);
+        ctx.stroke();
+        console.log(x, y);
+    });
+};
